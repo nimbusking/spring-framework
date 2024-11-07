2a30a9a05a18e944af727874b3a4390f4873c2c8
nimbusking <kemivong@hotmail.com>
2024-11-07 18:26:44 +0800

    添加IOC项目内gradle编译依赖模块

---
+++ spring-nimbusk-ioc/src/main/java/cc/nimbusk/beans/HelloWordService.java
@@ -0,0 +1,12 @@
+package cc.nimbusk.beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWordService {
+
+	public void sayHello() {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ spring-nimbusk-ioc/src/main/java/cc/nimbusk/main/IocMainTest.java
@@ -0,0 +1,19 @@
+package cc.nimbusk.main;
+
+import cc.nimbusk.beans.HelloWordService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cc.nimbusk")
+public class IocMainTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(IocMainTest.class);
+		HelloWordService helloWordService = context.getBean(HelloWordService.class);
+		helloWordService.sayHello();
+	}
+
+}
